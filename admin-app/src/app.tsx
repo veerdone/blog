47bd988bebcd8ce116cbe05289f7facc56c7c5b2
yu <13112433432@163.com>
2022-04-19 09:26:01 +0000

    管理后台

---
+++ admin-app/src/app.tsx
@@ -0,0 +1,69 @@
+import {PageLoading} from '@ant-design/pro-layout';
+import type {RunTimeLayoutConfig} from 'umi';
+import {history} from 'umi';
+import RightContent from '@/components/RightContent';
+import Cookies from "js-cookie";
+import {message} from "antd";
+
+const loginPath = '/login';
+
+/** 获取用户信息比较慢的时候会展示一个 loading */
+export const initialStateConfig = {
+	loading: <PageLoading/>,
+};
+
+/**
+ * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
+ * */
+export async function getInitialState(): Promise<{
+	currentUser?: User
+	fetchUserInfo?: Function
+}> {
+	const fetchUserInfo = () => {
+		const user = Cookies.get("currentUser");
+		if (user) {
+			return JSON.parse(user);
+		}
+		return undefined;
+	};
+	// 如果不是登录页面，执行
+	if (history.location.pathname !== loginPath) {
+		const currentUser = fetchUserInfo();
+		return {
+			currentUser,
+			fetchUserInfo,
+		};
+	}
+	return {
+		fetchUserInfo,
+	};
+}
+
+// ProLayout 支持的api https://procomponents.ant.design/components/layout
+export const layout: RunTimeLayoutConfig = ({initialState, setInitialState}) => {
+	return {
+		rightContentRender: () => <RightContent/>,
+		disableContentMargin: false,
+		onPageChange: () => {
+			const {location} = history;
+			const currentUser = initialState?.fetchUserInfo?.();
+			// 如果没有登录，重定向到 login
+			if (!initialState?.currentUser && !currentUser && location.pathname !== loginPath) {
+				message.warning("请先登录!");
+				history.push(loginPath);
+			}
+		},
+		menuHeaderRender: undefined,
+		// 自定义 403 页面
+		// unAccessible: <div>unAccessible</div>,
+		// 增加一个 loading 的状态
+		childrenRender: (children) => {
+			// if (initialState?.loading) return <PageLoading />;
+			return (
+				<>
+					{children}
+				</>
+			);
+		},
+	};
+};
